b3950bc8ce13e3425f6fd6a3e1f811e65427303e
Ancuta Ioan <77390188+AncutaIoan@users.noreply.github.com>
2022-05-07 23:23:07 +0300

    Update UIImageView.swift

--- CartOS/CartOS/Extensions/UIImageView.swift
+++ CartOS/CartOS/Extensions/UIImageView.swift
@@ -11,7 +11,7 @@ extension UIImageView {
     // Round shaped image
     func makeRounded() {
         self.layer.masksToBounds = false
-        //self.layer.cornerRadius = self.frame.height / 2
+        self.layer.cornerRadius = self.frame.height / 2
         self.clipsToBounds = true
     }
 }
